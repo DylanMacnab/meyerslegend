a43e0e78a56fdbf901ba29b719ab5b342f07d720
Dylan_Macnab <dylanmacnab@gmail.com>
2018-04-30 22:21:49 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key:         'bsnAifZGURxqnrDkG8mueAzIR',
+  consumer_secret:      'lBhlxkmu0h3kcr7RB6H4W6aCaNqd9sKOKEWrClMasJrHPgYDCk',
+  access_token:         '990964059284553734-ZFw22bWtHJU6XWoyWQem97jfHKkvmY4',
+  access_token_secret:  'GQASgeoYzgtoV8Kla4Haa58UtzlIVJ5nMHHJAmQiFsIdS',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
---
+++ notes.md
@@ -0,0 +1,5 @@
+# TIL
+
+1. You can create shorthand names for npm scripts or commands in the package.json. For example, 'npm start' will run 'node index.js';
+2. The node modules folder is where all our dependencies are stored.
+3. There is a require() module on the global level of npm that allows you to import npm modules into a file.
