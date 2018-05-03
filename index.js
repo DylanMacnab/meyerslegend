870b62a622630c61440f08767eccff78b042f154
Dylan_Macnab <dylanmacnab@gmail.com>
2018-05-02 23:02:44 -0700

    adding simple get search and post tweet examples

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ var T = new Twit(config);
 
 // GET Example
 
-T.get('search/tweets', { q: 'donald trump', count: 3 }, function(err, data, response) {
-  var tweets = data.statuses[0].text;
-  console.log(tweets)
+// T.get('search/tweets', { q: 'meyers leonard', count: 3 }, function(err, data, response) {
+//   var tweets = data.statuses;
+//   tweets.forEach((tweet, i) => {
+//    console.log(tweet.text)
+//   });
+// });
+
+
+// POST Example
+T.post('statuses/update', { status: 'this tweet is coming to you somewhere on spaceship earth.'}, function(err, data, response) {
+  console.log(data.text);
 });
