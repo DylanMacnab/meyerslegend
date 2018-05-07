9f25fc5bcdfc531b2837ec7ad464d8ce326f0094
Dylan_Macnab <dylanmacnab@gmail.com>
2018-05-06 20:34:52 -0700

    first commit

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-// GET Example
 
+
+
+// GET Example
+/*
+Simple git example showing how to search for people that tweeted "mayers leonard"
+This enpoint returns 3 tweets and calls a callback function.
+*/
 // T.get('search/tweets', { q: 'meyers leonard', count: 3 }, function(err, data, response) {
 //   var tweets = data.statuses;
 //   tweets.forEach((tweet, i) => {
@@ -15,8 +21,18 @@ var T = new Twit(config);
 // });
 
 /*
-// POST Example //
 
+
+
+
+
+// POST Example //
+/*
+Post a tweet example with a random number.
+Takes post type, data, and callback function with error catching.
+*/
+
+/*
 // run the tweet it once before it runs
 tweetIt();
 // run this tweetIt function at this interval
@@ -37,10 +53,25 @@ function tweetIt() {
 }
 */
 
-// STREAM Example
 
+
+
+
+
+// STREAM Example:
+/*
+In this example when someone follows this account
+the bot will tweet at the new follower using the
+event of the on('follow') event that gets passed
+via the stream. The callback function followed is
+called and the tweetIt function is passed data to
+reply to the new follower.
+*/
+/*
 // create stream
 var stream = T.stream('user');
+// when someone follows (event handler on twitter stream with callback)
+stream.on('follow', followed);
 
 function followed(eventMsg) {
   var name = eventMsg.source.name;
@@ -60,8 +91,47 @@ function tweetIt(name, screenName) {
     }
   });
 }
-
-// anytime someone follows me -- event handler on the stream with followed callback
-stream.on('follow', followed);
+*/
 
 
+
+
+
+// Reply Bot Example
+/*
+This example shows how to write use the tweet event to reply to a user
+*/
+
+// set twit stream user connection
+var stream = T.stream('user');
+// tweet event handler
+stream.on('tweet', tweetEvent);
+
+function tweetEvent(eventMsg) {
+  // creates tween.json file with eventMsg data
+  // var fs = require('fs');
+  // var json = JSON.stringify(eventMsg , null, 2);
+  // fs.writeFile("tweet.json", json);
+
+  var replyTo = eventMsg.in_reply_to_screen_name;
+  var text = eventMsg.text;
+  var from = eventMsg.user.screen_name;
+  // only responds if tweet was a reply to my account
+  if (replyTo === 'meyers_legend') {
+    var newTweet = `@${from} thank you for tweeting me!`;
+    tweetIt(newTweet)
+  }
+}
+
+function tweetIt(text) {
+  var tweet = {
+    status: text
+  }
+  T.post('statuses/update', tweet, (err, data, response) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(tweet);
+    }
+  });
+}
